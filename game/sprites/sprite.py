d113b16ce1e55379b659f57a00fc7da2dbf7a4d3
fordaaaa <foworda@gmail.com>
2025-09-28 11:02:42 -0400

    added the new sprites and updated sprite.py

--- game/sprites/sprite.py
+++ game/sprites/sprite.py
@@ -4,7 +4,16 @@ from game.sprites.spritesheet import SpriteSheet
 
 class Sprite():
 	SPRITESHEETS: dict[str, SpriteSheet] = {
-	"player_bodies": SpriteSheet("spritesheet.png", {"a": (0, 0, 32, 32), "b": (32, 0, 32, 32)}, 4)
+	"player_bodies": SpriteSheet("spritesheet.png", {
+		"a": (128, 0, 32, 32),
+		"b": (160, 0, 32, 32)
+	}, 4),
+	"weapons": SpriteSheet("spritesheet.png", {
+		"assault_rifle": (0, 32, 32, 32),
+		"bullet": (32, 32, 32, 32),
+		"lmg": (64, 32, 32, 32),
+		"revolver": (96, 32, 32, 32)
+	}, 4)
 }
 	
 	def __init__(self, spritesheet: str, sprite_name: str):
