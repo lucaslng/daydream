232ebc41c81ddc6a501c8be89fff53a899f3dc57
fordaaaa <foworda@gmail.com>
2025-09-28 09:02:37 -0400

    Update game.py

--- game/game.py
+++ game/game.py
@@ -10,6 +10,7 @@ from game.ecs.systems.input import InputSystem
 from game.ecs.systems.movement import MovementSystem
 from game.ecs.systems.render import RenderSystem
 from game.sprites.sprite import Sprite
+from game.background import load_game_background, create_game_overlay
 from util.prepare import CLOCK, SURF
 from util.screens import Screens
 from util.update_screen import update_screen
@@ -22,10 +23,14 @@ async def game() -> Screens:
 
 	enemy = Entity()
 	enemy.add_components(Position(300, 100), Velocity(0, 0), Speed(50), PersonSprite(Sprite("player_bodies", "b")), PlayerComponent(), Collider(128, 128), Rotation(20))
+	
 	entities = [player, enemy]
 	input_system = InputSystem()
 	movement_system = MovementSystem()
 	render_system = RenderSystem()
+	
+	background = load_game_background()
+	overlay = create_game_overlay()
 	
 	while True:
 		dt = CLOCK.tick(60) / 1000.0
@@ -34,12 +39,18 @@ async def game() -> Screens:
 			if event.type == pg.QUIT:
 				raise SystemExit
 			if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
-				return Screens.MENU
+				return Screens.INGAMEMENU
+			if event.type == pg.KEYDOWN and event.key == pg.K_i:
+				return Screens.GAMEOVER
+			if event.type == pg.KEYDOWN and event.key == pg.K_o:
+				return Screens.LEVELCLEAR
 			
 		input_system.update(entities, player, dt)
 		movement_system.update(entities, player, dt)
+		
 
-		SURF.fill((255, 0, 0))
+		SURF.blit(background, (0, 0))
+		SURF.blit(overlay, (0, 0))
 
 		render_system.update(entities, player, dt)
 
