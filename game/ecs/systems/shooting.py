446b4ff7bb285c526b27877bcecf361064d6517d
fordaaaa <foworda@gmail.com>
2025-09-28 11:19:31 -0400

    fix stuff

--- game/ecs/systems/shooting.py
+++ game/ecs/systems/shooting.py
@@ -11,22 +11,20 @@ from game.ecs.entity import Entity
 
 class ShootingSystem:
 	def __init__(self):
-		self.bullet_speed = 800
-		self.bullet_scale = 0.75  # 25% smaller (75% of original size)
+		self.bullet_speed = 1000
+		self.bullet_scale = 0.75  #scaled down might change later
 	
 	def create_bullet(self, player_pos: Position, player_rotation: Rotation) -> Entity:
 		"""Create a bullet that shoots forward from the player's position"""
-		# Calculate bullet direction based on player rotation (same as dash system)
 		bullet_dx = self.bullet_speed * cos(radians(player_rotation.angle))
 		bullet_dy = self.bullet_speed * -sin(radians(player_rotation.angle))
 		
-		# Create bullet entity with scaled sprite
 		bullet = Entity()
 		bullet.add_components(
 			Position(player_pos.x, player_pos.y),
 			Velocity(bullet_dx, bullet_dy),
 			PersonSprite(Sprite("weapons", "bullet")),
-			Collider(12, 12),  # Smaller collision box for 25% scaled bullet
+			Collider(12, 12),  #scaled collision box !!!!!!!!!!! sync with self.bullet_scale please
 			Bullet(),
 			Rotation(player_rotation.angle)
 		)
